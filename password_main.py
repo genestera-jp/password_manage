9eb9efd26d75f1b9b7d98a9eb40779c07b10cc73
genestera <genesistera@mediacat.ne.jp>
2018-09-12 09:57:50 +0900

    rebersion 1.0.1
    lisit add

--- password_main.py
+++ password_main.py
@@ -8,12 +8,14 @@ class Application(tk.Frame):
         super().__init__(master)
         self.grid()
         self.create_widgets()
-        master.title(u"テスト")
-        master.geometry("400x320")
+        master.title(u"パスワード一覧")
+        master.geometry("512x512")
+        self.id_list = []
+        self.pass_list = []
 
     def create_widgets(self):
         #リストボックス
-        self.ListBox1 = tk.Listbox(self,height=10,width=30)
+        self.ListBox1 = tk.Listbox(self,height=16,width=48)
         self.ListBox1.grid(column=0,row=0,rowspan=4,padx=5,pady=5)
         self.ListBox1.bind("<Delete>",self.Delete_press)
         self.ListBox1.bind("<Shift-Delete>",self.Ins_press)
@@ -21,10 +23,23 @@ class Application(tk.Frame):
         self.scrollbar1 = tk.Scrollbar(self,orient=tk.VERTICAL,command=self.ListBox1.yview)
         self.ListBox1['yscrollcommand'] = self.scrollbar1.set
         self.scrollbar1.grid(column=1,row=0,rowspan=4,sticky=tk.W+tk.N+tk.S)
-        #入力ボックス
+        #入力ボックス１
+        self.Label2 = tk.Label(text=u'Name')
+        self.Label2.grid(column=2,row=0,pady=2,sticky=tk.N)
         self.TextBox1 = tk.Entry(width=25)
-        self.TextBox1.grid(column=2,row=0)
+        self.TextBox1.grid(column=2,row=0,pady=24,sticky=tk.N)
         self.TextBox1.bind("<Return>",self.enter_press)
+        #入力ボックス２
+        self.Label3 = tk.Label(text=u'ID')
+        self.Label3.grid(column=2,row=0,pady=42,sticky=tk.N)
+        self.TextBox2 = tk.Entry(width=25)
+        self.TextBox2.grid(column=2,row=0,pady=64,sticky=tk.N)
+        #入力ボックス３
+        self.Label4 = tk.Label(text=u'Password')
+        self.Label4.grid(column=2,row=0,pady=82,sticky=tk.N)
+        self.TextBox3 = tk.Entry(width=25)
+        self.TextBox3.grid(column=2,row=0,pady=104,sticky=tk.N)
+
         #ボタン１
         self.Button1 = tk.Button(text=u'入力ボタン',width=20,command=self.button_click)
         self.Button1.grid(column=2,row=1,columnspan=2,pady=5)
@@ -36,7 +51,7 @@ class Application(tk.Frame):
         self.Button3.grid(column=2,row=3,columnspan=2,pady=5)
         #ラベル
         self.Label1 = tk.Label(text=u'test')
-        self.Label1.grid(column=0,row=5,columnspan=2,pady=10,sticky=W)
+        self.Label1.grid(column=0,row=5,columnspan=2,pady=10,sticky=tk.W)
 
     #入力の関数
     def button_click(self):
@@ -44,6 +59,11 @@ class Application(tk.Frame):
         self.Label1["text"] = Val + " を入力しました"
         self.ListBox1.insert(tk.END,Val)
         self.TextBox1.delete(0,tk.END)
+        Val = self.TextBox2.get()
+        self.id_list.append(Val)
+        Val = self.TextBox3.get()
+        self.pass_list.append(Val)
+        self.Label1["text"] = self.id_list + self.pass_list
 
     #ファイル保存の関数
     def button_click1(self):
@@ -81,6 +101,7 @@ class Application(tk.Frame):
         self.TextBox1.delete(0,tk.END)
 
 #本体
-root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    app = Application(master=root)
+    app.mainloop()
